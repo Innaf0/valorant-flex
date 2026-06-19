79a7de766f8065f12df577f9341ef9b7e6934bb6
Innaf0 <fanni.balazs.2012@gmail.com>
2026-06-19 21:15:50 +0300

    started on firmware

---
+++ firmware/src/main.rs
@@ -0,0 +1,12 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+}
